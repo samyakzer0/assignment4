60f3edc5a6efb520ab979399a00ea791dfdda212
SMYK <samyak.sage@gmail.com>
2026-03-03 17:47:38 +0530

    Added linear search

---
+++ Linear.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+int linearSearch(int arr[], int n, int target){
+	for (int i = 0 ; i< n ; i++){
+		if(arr[i]==target){
+			return i;
+		}
+	}
+	return -1;
+}
+int main() {
+	int n;
+	cin>>n;
+
+	int arr[n];
+	for (int i = 0 ; i<n ; i++){
+		cin>>arr[i];
+	}
+	int target;
+	cin>>target;
+
+	int index = linearSearch(arr, n, target);
+	cout << index << endl;
+
+	return 0;
+}
