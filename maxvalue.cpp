1219624c0f35cd8885dccb3a03e74af04f3b6ee4
SMYK <samyak.sage@gmail.com>
2026-03-06 13:27:53 +0530

    Add maxvalue.cpp

---
+++ maxvalue.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int maxValue(int arr[], int n){
+	int max = arr[0];
+	for (int i = 1 ; i<n ; i++){
+		if (max<arr[i]){
+			max = arr[i];
+		}
+	}
+	return max;
+}
+int main() {
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i = 0 ; i<n ; i++){
+		cin >> arr[i];
+	}
+
+	int maximum = maxValue(arr,n);
+	cout << maximum << endl;
+	return 0;
+}
