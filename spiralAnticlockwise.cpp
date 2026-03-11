d9aab3a6f37a9e168df6dbd5b451cb736bbf8d74
SMYK <samyak.sage@gmail.com>
2026-03-11 14:02:41 +0530

    added

---
+++ spiralAnticlockwise.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    vector<vector<int>> arr(n, vector<int>(m));
+
+    int left = 0 ;
+    int right = m-1;
+    int top = 0;
+    int bottom = n-1;
+
+    while(left<=right && top<=bottom){
+        for(int i = right ; i >= left ; i--){
+            cout << arr[top][i] << " ";
+        }
+        top++;
+
+        for(int i = top ; i<=bottom ; i++){
+            cout <<arr[i][right]<< " ";
+        }
+        bottom--;
+
+        for(int i = left ; i<= right ; i++){
+            cout << arr[bottom][i]  << " ";
+        }
+        left++;
+
+        for(int i = bottom ; i>= top ; i--){
+            cout << arr[i][left] << " ";
+        }
+            right--;
+
+            
+    }
+    return 0;
+}
